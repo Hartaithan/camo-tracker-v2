71e4b9906098aaaf477a9ec8f318bbe57b25511f
Hartaithan <hartaithan@gmail.com>
2021-08-12 19:18:14 +0900

    feat: redux only on dev

--- client/src/redux/index.js
+++ client/src/redux/index.js
@@ -3,7 +3,8 @@ import { loadState, saveState } from "../localStorage";
 import rootReducer from "./reducers";
 
 const persistedState = loadState();
-const store = createStore(rootReducer, persistedState, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const devTools = process.env.NODE_ENV === "production" && window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+const store = createStore(rootReducer, persistedState, devTools);
 
 store.subscribe(() => {
 	saveState(store.getState());
