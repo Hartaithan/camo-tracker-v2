ba8e90cc960458c016f5535b0c121e922088a37b
Hartaithan <hartaithan@gmail.com>
2021-08-12 20:01:25 +0900

    fix: redux devtools error

--- client/src/redux/index.js
+++ client/src/redux/index.js
@@ -3,7 +3,7 @@ import { loadState, saveState } from "../localStorage";
 import rootReducer from "./reducers";
 
 const persistedState = loadState();
-const devTools = process.env.NODE_ENV === "production" && window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+const devTools = process.env.NODE_ENV !== 'production' && (window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 const store = createStore(rootReducer, persistedState, devTools);
 
 store.subscribe(() => {
