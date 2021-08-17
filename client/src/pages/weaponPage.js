7e01938ce0c4603c12ef1082197393ad94397c24
Hartaithan <hartaithan@gmail.com>
2021-08-17 15:48:20 +0900

    feat: check if previous camo is unlocked

--- client/src/pages/weaponPage.js
+++ client/src/pages/weaponPage.js
@@ -7,192 +7,288 @@ import db_da_mission from "../data/db_da_mission.json";
 import { useSelector, useDispatch } from "react-redux";
 import { useLocation } from "react-router-dom";
 import Header from "../components/headerComponent";
+import { toast } from "react-hot-toast";
 
 function WeaponPage() {
-	const paths = useLocation().pathname.split("/");
-	const [id_mast, id_cat, id_weap] = paths[2].split("_");
+  const paths = useLocation().pathname.split("/");
+  const [id_mast, id_cat, id_weap] = paths[2].split("_");
 
-	const main = useSelector((state) => state.main);
-	const camos = id_mast === "dm" ? db_dm_camo : db_da_camo;
-	const mission = id_mast === "dm" ? db_dm_mission : db_da_mission;
-	const app = useSelector((state) => state.app);
-	const dispatch = useDispatch();
+  const main = useSelector((state) => state.main);
+  const camos = id_mast === "dm" ? db_dm_camo : db_da_camo;
+  const mission = id_mast === "dm" ? db_dm_mission : db_da_mission;
+  const app = useSelector((state) => state.app);
+  const dispatch = useDispatch();
 
-	var style = {
-		isOpen: {
-			width: "calc(100% - 300px)",
-			marginLeft: "300px",
-		},
-		isClosed: {
-			marginLeft: "0px",
-		},
-	};
+  var style = {
+    isOpen: {
+      width: "calc(100% - 300px)",
+      marginLeft: "300px",
+    },
+    isClosed: {
+      marginLeft: "0px",
+    },
+  };
 
-	function getMission(id_camo_cat, id_camo) {
-		const goal = mission[id_cat - 1].weapons[id_weap - 1].camos[id_camo_cat - 1].goals[id_camo];
-		const name = mission[id_cat - 1].weapons[id_weap - 1].name;
-		const getMission = mission[id_cat - 1].weapons[id_weap - 1].camos[id_camo_cat - 1].mission.replace(/_goal/i, goal).replace(/_name/i, name);
-		return getMission;
-	}
+  function getMission(id_camo_cat, id_camo) {
+    const goal =
+      mission[id_cat - 1].weapons[id_weap - 1].camos[id_camo_cat - 1].goals[
+        id_camo
+      ];
+    const name = mission[id_cat - 1].weapons[id_weap - 1].name;
+    const getMission = mission[id_cat - 1].weapons[id_weap - 1].camos[
+      id_camo_cat - 1
+    ].mission
+      .replace(/_goal/i, goal)
+      .replace(/_name/i, name);
+    return getMission;
+  }
 
-	function getMasteryMission(id_camo_cat, id_camo) {
-		const name = mission[id_cat - 1].weapons[id_weap - 1].name;
-		const getMasteryMission = mission[id_cat - 1].weapons[id_weap - 1].camos[id_camo_cat - 1].mission[id_camo].replace(/_name/i, name);
-		return getMasteryMission;
-	}
+  function getMasteryMission(id_camo_cat, id_camo) {
+    const name = mission[id_cat - 1].weapons[id_weap - 1].name;
+    const getMasteryMission = mission[id_cat - 1].weapons[id_weap - 1].camos[
+      id_camo_cat - 1
+    ].mission[id_camo].replace(/_name/i, name);
+    return getMasteryMission;
+  }
 
-	function calcPerc(id_camocat) {
-		let array = [];
-		if (id_camocat === 1) {
-			array = main[id_cat - 1].weapons[id_weap - 1].camos[id_mast].slice(0, 5);
-		} else if (id_camocat === 2) {
-			array = main[id_cat - 1].weapons[id_weap - 1].camos[id_mast].slice(5, 10);
-		} else if (id_camocat === 3) {
-			array = main[id_cat - 1].weapons[id_weap - 1].camos[id_mast].slice(10, 15);
-		} else if (id_camocat === 4) {
-			array = main[id_cat - 1].weapons[id_weap - 1].camos[id_mast].slice(15, 20);
-		} else if (id_camocat === 5) {
-			array = main[id_cat - 1].weapons[id_weap - 1].camos[id_mast].slice(20, 25);
-		} else if (id_camocat === 6) {
-			array = main[id_cat - 1].weapons[id_weap - 1].camos[id_mast].slice(25, 30);
-		} else if (id_camocat === 7) {
-			array = main[id_cat - 1].weapons[id_weap - 1].camos[id_mast].slice(30, 35);
-		} else if (id_camocat === 8) {
-			array = main[id_cat - 1].weapons[id_weap - 1].camos[id_mast];
+  function calcPerc(id_camocat) {
+    let array = [];
+    if (id_camocat === 1) {
+      array = main[id_cat - 1].weapons[id_weap - 1].camos[id_mast].slice(0, 5);
+    } else if (id_camocat === 2) {
+      array = main[id_cat - 1].weapons[id_weap - 1].camos[id_mast].slice(5, 10);
+    } else if (id_camocat === 3) {
+      array = main[id_cat - 1].weapons[id_weap - 1].camos[id_mast].slice(
+        10,
+        15
+      );
+    } else if (id_camocat === 4) {
+      array = main[id_cat - 1].weapons[id_weap - 1].camos[id_mast].slice(
+        15,
+        20
+      );
+    } else if (id_camocat === 5) {
+      array = main[id_cat - 1].weapons[id_weap - 1].camos[id_mast].slice(
+        20,
+        25
+      );
+    } else if (id_camocat === 6) {
+      array = main[id_cat - 1].weapons[id_weap - 1].camos[id_mast].slice(
+        25,
+        30
+      );
+    } else if (id_camocat === 7) {
+      array = main[id_cat - 1].weapons[id_weap - 1].camos[id_mast].slice(
+        30,
+        35
+      );
+    } else if (id_camocat === 8) {
+      array = main[id_cat - 1].weapons[id_weap - 1].camos[id_mast];
+    }
+    const percentage = (array.filter(Boolean).length / array.length) * 100;
+    return Math.round(percentage) + "%";
+  }
+
+  function checkMastery(id_camo) {
+    // CHECK GOLD
+    if (id_camo === 36) {
+      return main[id_cat - 1].weapons[id_weap - 1].completed[id_mast] ? 0 : 1;
+      // CHECK DIAMOND
+    } else if (id_camo === 37) {
+      if (main[id_cat - 1].weapons[id_weap - 1].dlc) {
+        if (main[id_cat - 1].weapons[id_weap - 1].completed[id_mast]) {
+          return main[id_cat - 1].completed[id_mast] ? 0 : 1;
+        }
+      } else {
+        return main[id_cat - 1].completed[id_mast] ? 0 : 1;
+      }
+      // CHECK MASTERY
+    } else if (id_camo === 38) {
+      let n = 0;
+      main.map((weapon) => {
+        if (weapon.completed[id_mast] === true) {
+          n++;
+        }
+        return n;
+      });
+      if (main[id_cat - 1].weapons[id_weap - 1].dlc) {
+        if (main[id_cat - 1].weapons[id_weap - 1].completed[id_mast]) {
+          return n === main.length ? 0 : 1;
+        }
+      } else {
+        return n === main.length ? 0 : 1;
+      }
+    }
+  }
+
+  const [show, setShow] = React.useState([
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+  ]);
+
+  function setShowCheck(index) {
+    if (index !== 7) {
+      show[index] = !show[index];
+      setShow([...show, show]);
+    }
+  }
+
+  function setCamo(id_camo, index, i) {
+		const type = {
+			type: "TOGGLE_CAMO",
+			id_cat: id_cat,
+			id_weap: id_weap,
+			id_camo: id_camo,
+			id_mast: id_mast,
 		}
-		const percentage = (array.filter(Boolean).length / array.length) * 100;
-		return Math.round(percentage) + "%";
-	}
+		// ПРОВЕРКА НА ТО ПЕРВЫЙ ЛИ ЭТО КАМО В КАТЕГОРИИ ИЛИ НЕТ
+    if (index === 0) {
+      dispatch(type);
+      return;
+    } else {
+			// РАЗДЕЛЕНИЕ САМО АРРЕЯ НА 7 ЧАСТЕЙ ПО 5 ЭЛЕМЕНТОВ
+      const array = [...main[id_cat - 1].weapons[id_weap - 1].camos[id_mast]];
+      let categories = [];
+      while (array.length > 0) {
+        categories.push(array.splice(0, 5));
+      }
+      const getCategoryArray = categories[i];
+      if (getCategoryArray[index - 1]) {
+        dispatch(type);
+      } else {
+        toast.error("Previous camo is not unlocked.");
+      }
+    }
+  }
 
-	function checkMastery(id_camo) {
-		// CHECK GOLD
-		if (id_camo === 36) {
-			return main[id_cat - 1].weapons[id_weap - 1].completed[id_mast] ? 0 : 1;
-			// CHECK DIAMOND
-		} else if (id_camo === 37) {
-			if (main[id_cat - 1].weapons[id_weap - 1].dlc) {
-				if (main[id_cat - 1].weapons[id_weap - 1].completed[id_mast]) {
-					return main[id_cat - 1].completed[id_mast] ? 0 : 1;
-				}
-			} else {
-				return main[id_cat - 1].completed[id_mast] ? 0 : 1;
-			}
-			// CHECK MASTERY
-		} else if (id_camo === 38) {
-			let n = 0;
-			main.map((weapon) => {
-				if (weapon.completed[id_mast] === true) {
-					n++;
-				}
-				return n;
-			});
-			if (main[id_cat - 1].weapons[id_weap - 1].dlc) {
-				if (main[id_cat - 1].weapons[id_weap - 1].completed[id_mast]) {
-					return n === main.length ? 0 : 1;
-				}
-			} else {
-				return n === main.length ? 0 : 1;
-			}
-		}
-	}
-
-	const [show, setShow] = React.useState([false, false, false, false, false, false, false, false]);
-
-	function setShowCheck(index) {
-		if (index !== 7) {
-			show[index] = !show[index];
-			setShow([...show, show]);
-		}
-	}
-
-	return (
-		<div className="tracker_container" style={app.isOpen ? style.isOpen : style.isClosed}>
-			<Header />
-			<div className="tracker_weapon">
-				<div className="tracker_weapon_wrapper">
-					{camos.map((item, index) => (
-						<div className="tracker_weapon_wrapper_container" onMouseEnter={() => setShowCheck(index)} onMouseLeave={() => setShowCheck(index)} key={item.id}>
-							<div className="tracker_weapon_wrapper_container_text">
-								<div className="tracker_weapon_wrapper_container_text_name">{item.name.toUpperCase()}</div>
-								{index !== 7 && (
-									<div className="tracker_weapon_wrapper_container_text_check" style={{ opacity: show[index] ? "1" : "0" }}>
-										<svg
-											onClick={() =>
-												dispatch({
-													type: "TOGGLE_CAMO_CATEG",
-													id_cat: id_cat,
-													id_weap: id_weap,
-													id_camo_cat: index + 1,
-													id_mast: id_mast,
-												})
-											}
-											version="1.1"
-											xmlns="http://www.w3.org/2000/svg"
-											x="0px"
-											y="0px"
-											viewBox="0 0 448 448"
-										>
-											<polygon points="341.333,192.96 341.333,381.12 42.667,381.12 42.667,82.453 308.693,82.453 355.627,39.787 0,39.787 0,423.787 384,423.787 384,153.707" />
-											<polygon points="417.493,24.213 186.027,234.56 113.493,162.24 85.333,190.4 187.947,293.013 448,54.08" />
-										</svg>
-									</div>
-								)}
-								<div className="tracker_weapon_wrapper_container_text_percentage">{calcPerc(item.id)}</div>
-							</div>
-							<div className="tracker_weapon_wrapper_container_bar">
-								<div className="tracker_weapon_wrapper_container_bar_yellow" style={{ width: calcPerc(item.id) }}></div>
-							</div>
-							<div className="tracker_weapon_wrapper_container_camocontainer">
-								{item.camos.map((camo, index) => {
-									if (camo.id === 36 || camo.id === 37 || camo.id === 38) {
-										return (
-											<div className={`tracker_weapon_wrapper_container_camocontainer_camo tracker_weapon_wrapper_container_camocontainer_master`} data-title={getMasteryMission(item.id, index)} key={camo.id}>
-												<div className="tracker_weapon_wrapper_container_camocontainer_camo_lock" style={{ opacity: checkMastery(camo.id) }}>
-													<svg viewBox="-64 0 512 512" xmlns="http://www.w3.org/2000/svg">
-														<path d="m336 192h-16v-64c0-70.59375-57.40625-128-128-128s-128 57.40625-128 128v64h-16c-26.453125 0-48 21.523438-48 48v224c0 26.476562 21.546875 48 48 48h288c26.453125 0 48-21.523438 48-48v-224c0-26.476562-21.546875-48-48-48zm-229.332031-64c0-47.0625 38.269531-85.332031 85.332031-85.332031s85.332031 38.269531 85.332031 85.332031v64h-170.664062zm0 0" />
-													</svg>
-												</div>
-												<img className="tracker_weapon_wrapper_container_camocontainer_camo_img" src={camo.img} alt="camo_img" />
-											</div>
-										);
-									} else {
-										return (
-											<div
-												className="tracker_weapon_wrapper_container_camocontainer_camo"
-												onClick={() =>
-													dispatch({
-														type: "TOGGLE_CAMO",
-														id_cat: id_cat,
-														id_weap: id_weap,
-														id_camo: camo.id,
-														id_mast: id_mast,
-													})
-												}
-												key={camo.id}
-												data-title={getMission(item.id, index)}
-											>
-												<div
-													className="tracker_weapon_wrapper_container_camocontainer_camo_lock"
-													style={{
-														opacity: main[id_cat - 1].weapons[id_weap - 1].camos[id_mast][camo.id - 1] ? 0 : 1,
-													}}
-												>
-													<svg viewBox="-64 0 512 512" xmlns="http://www.w3.org/2000/svg">
-														<path d="m336 192h-16v-64c0-70.59375-57.40625-128-128-128s-128 57.40625-128 128v64h-16c-26.453125 0-48 21.523438-48 48v224c0 26.476562 21.546875 48 48 48h288c26.453125 0 48-21.523438 48-48v-224c0-26.476562-21.546875-48-48-48zm-229.332031-64c0-47.0625 38.269531-85.332031 85.332031-85.332031s85.332031 38.269531 85.332031 85.332031v64h-170.664062zm0 0" />
-													</svg>
-												</div>
-												<img className="tracker_weapon_wrapper_container_camocontainer_camo_img" src={camo.img} alt="camo_img" />
-											</div>
-										);
-									}
-								})}
-							</div>
-						</div>
-					))}
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div
+      className="tracker_container"
+      style={app.isOpen ? style.isOpen : style.isClosed}
+    >
+      <Header />
+      <div className="tracker_weapon">
+        <div className="tracker_weapon_wrapper">
+          {camos.map((item, i) => (
+            <div
+              className="tracker_weapon_wrapper_container"
+              onMouseEnter={() => setShowCheck(i)}
+              onMouseLeave={() => setShowCheck(i)}
+              key={item.id}
+            >
+              <div className="tracker_weapon_wrapper_container_text">
+                <div className="tracker_weapon_wrapper_container_text_name">
+                  {item.name.toUpperCase()}
+                </div>
+                {i !== 7 && (
+                  <div
+                    className="tracker_weapon_wrapper_container_text_check"
+                    style={{ opacity: show[i] ? "1" : "0" }}
+                  >
+                    <svg
+                      onClick={() =>
+                        dispatch({
+                          type: "TOGGLE_CAMO_CATEG",
+                          id_cat: id_cat,
+                          id_weap: id_weap,
+                          id_camo_cat: i + 1,
+                          id_mast: id_mast,
+                        })
+                      }
+                      version="1.1"
+                      xmlns="http://www.w3.org/2000/svg"
+                      x="0px"
+                      y="0px"
+                      viewBox="0 0 448 448"
+                    >
+                      <polygon points="341.333,192.96 341.333,381.12 42.667,381.12 42.667,82.453 308.693,82.453 355.627,39.787 0,39.787 0,423.787 384,423.787 384,153.707" />
+                      <polygon points="417.493,24.213 186.027,234.56 113.493,162.24 85.333,190.4 187.947,293.013 448,54.08" />
+                    </svg>
+                  </div>
+                )}
+                <div className="tracker_weapon_wrapper_container_text_percentage">
+                  {calcPerc(item.id)}
+                </div>
+              </div>
+              <div className="tracker_weapon_wrapper_container_bar">
+                <div
+                  className="tracker_weapon_wrapper_container_bar_yellow"
+                  style={{ width: calcPerc(item.id) }}
+                ></div>
+              </div>
+              <div className="tracker_weapon_wrapper_container_camocontainer">
+                {item.camos.map((camo, index) => {
+                  if (camo.id === 36 || camo.id === 37 || camo.id === 38) {
+                    return (
+                      <div
+                        className={`tracker_weapon_wrapper_container_camocontainer_camo tracker_weapon_wrapper_container_camocontainer_master`}
+                        data-title={getMasteryMission(item.id, index)}
+                        key={camo.id}
+                      >
+                        <div
+                          className="tracker_weapon_wrapper_container_camocontainer_camo_lock"
+                          style={{ opacity: checkMastery(camo.id) }}
+                        >
+                          <svg
+                            viewBox="-64 0 512 512"
+                            xmlns="http://www.w3.org/2000/svg"
+                          >
+                            <path d="m336 192h-16v-64c0-70.59375-57.40625-128-128-128s-128 57.40625-128 128v64h-16c-26.453125 0-48 21.523438-48 48v224c0 26.476562 21.546875 48 48 48h288c26.453125 0 48-21.523438 48-48v-224c0-26.476562-21.546875-48-48-48zm-229.332031-64c0-47.0625 38.269531-85.332031 85.332031-85.332031s85.332031 38.269531 85.332031 85.332031v64h-170.664062zm0 0" />
+                          </svg>
+                        </div>
+                        <img
+                          className="tracker_weapon_wrapper_container_camocontainer_camo_img"
+                          src={camo.img}
+                          alt="camo_img"
+                        />
+                      </div>
+                    );
+                  } else {
+                    return (
+                      <div
+                        className="tracker_weapon_wrapper_container_camocontainer_camo"
+                        onClick={() => setCamo(camo.id, index, i)}
+                        key={camo.id}
+                        data-title={getMission(item.id, index)}
+                      >
+                        <div
+                          className="tracker_weapon_wrapper_container_camocontainer_camo_lock"
+                          style={{
+                            opacity: main[id_cat - 1].weapons[id_weap - 1]
+                              .camos[id_mast][camo.id - 1]
+                              ? 0
+                              : 1,
+                          }}
+                        >
+                          <svg
+                            viewBox="-64 0 512 512"
+                            xmlns="http://www.w3.org/2000/svg"
+                          >
+                            <path d="m336 192h-16v-64c0-70.59375-57.40625-128-128-128s-128 57.40625-128 128v64h-16c-26.453125 0-48 21.523438-48 48v224c0 26.476562 21.546875 48 48 48h288c26.453125 0 48-21.523438 48-48v-224c0-26.476562-21.546875-48-48-48zm-229.332031-64c0-47.0625 38.269531-85.332031 85.332031-85.332031s85.332031 38.269531 85.332031 85.332031v64h-170.664062zm0 0" />
+                          </svg>
+                        </div>
+                        <img
+                          className="tracker_weapon_wrapper_container_camocontainer_camo_img"
+                          src={camo.img}
+                          alt="camo_img"
+                        />
+                      </div>
+                    );
+                  }
+                })}
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default WeaponPage;
