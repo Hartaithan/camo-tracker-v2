725faa7fc823cb5992c0e343a5bda014edf98648
Hartaithan <hartaithan@gmail.com>
2021-08-19 21:05:44 +0900

    feat: add marshal pistol

--- client/src/redux/index.js
+++ client/src/redux/index.js
@@ -1,10 +1,10 @@
 import { createStore } from "redux";
-import { devToolsEnhancer } from 'redux-devtools-extension';
+import { devToolsEnhancer } from "redux-devtools-extension";
 import { loadState, saveState } from "../localStorage";
 import rootReducer from "./reducers";
 
 const persistedState = loadState();
-const devTools = process.env.NODE_ENV !== 'production' ? devToolsEnhancer({}) : undefined;
+const devTools = process.env.NODE_ENV !== "production" ? devToolsEnhancer({}) : undefined;
 const store = createStore(rootReducer, persistedState, devTools);
 
 store.subscribe(() => {
--- client/src/redux/reducers/userReducer.js
+++ client/src/redux/reducers/userReducer.js
@@ -1,34 +1,34 @@
 const initialState = {
-  token: null,
-  userId: null,
-  email: null,
-  nick: null,
-  isAuth: false,
+	token: null,
+	userId: null,
+	email: null,
+	nick: null,
+	isAuth: false,
 };
 
 const userReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case "LOG_IN":
-      return {
-        ...state,
-        token: action.token,
-        userId: action.userId,
-        email: action.email,
-        nick: action.nick,
-        isAuth: true,
-      };
-    case "LOG_OUT":
-      return {
-        ...state,
-        token: null,
-        userId: null,
-        email: null,
-        nick: null,
-        isAuth: false,
-      };
-    default:
-      return state;
-  }
+	switch (action.type) {
+		case "LOG_IN":
+			return {
+				...state,
+				token: action.token,
+				userId: action.userId,
+				email: action.email,
+				nick: action.nick,
+				isAuth: true,
+			};
+		case "LOG_OUT":
+			return {
+				...state,
+				token: null,
+				userId: null,
+				email: null,
+				nick: null,
+				isAuth: false,
+			};
+		default:
+			return state;
+	}
 };
 
 export default userReducer;
