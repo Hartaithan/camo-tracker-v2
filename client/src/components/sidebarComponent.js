5228af3af7f7b09af8d4d51306fd8d6d551d2cb8
Hartaithan <hartaithan@gmail.com>
2021-08-21 13:18:52 +0900

    feat: change react-collapsible to bootstrap-accordion in sidebar

--- client/src/components/sidebarComponent.js
+++ client/src/components/sidebarComponent.js
@@ -1,8 +1,8 @@
 import React from "react";
 import "../styles/sidebarComponent.scss";
-import Collapsible from "react-collapsible";
 import { Link } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
+import { Accordion, Tabs, Tab } from "react-bootstrap";
 
 function Sidebar() {
 	const items = useSelector((state) => state.main);
@@ -14,54 +14,6 @@ function Sidebar() {
 		if (width <= 768) {
 			dispatch({ type: "TOGGLE_SIDEBAR" });
 		}
-	}
-
-	function arrow(state, item) {
-		return (
-			<>
-				<svg className="tracker_arrow" style={state ? { transform: "rotate(90deg)" } : { transform: "rotate(0deg)" }} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-					<path d="M21 12l-18 12v-24z" />
-				</svg>
-				<span>{item.toUpperCase()}</span>
-			</>
-		);
-	}
-
-	function CollapsibleList(props) {
-		const collapsibleId = props.mast === "dm" ? app.collapsibleIsOpen.dm : app.collapsibleIsOpen.da;
-
-		return (
-			<div className="tracker_sidebar_container_collapse">
-				{items.map((item, index) => (
-					<div key={props.mast + item.name}>
-						<Collapsible
-							handleTriggerClick={() =>
-								dispatch({
-									type: "TOGGLE_COLLAPSIBLE",
-									mast: props.mast,
-									id: index,
-								})
-							}
-							open={collapsibleId[index] ? true : false}
-							trigger={collapsibleId[index] ? arrow(true, item.name) : arrow(false, item.name)}
-							transitionTime={100}
-						>
-							{item.weapons.map((weapon) => (
-								<div className="tracker_sidebar_container_collapse_listitem" key={props.mast + weapon.name}>
-									<span key={props.mast + weapon.name}>
-										&emsp;&emsp;
-										<Link to={"/weapon/" + props.mast + "_" + item.id + "_" + weapon.id} onClick={closeSidebar}>
-											{weapon.name.toUpperCase()}
-										</Link>
-										{weapon.dlc && <sup className="tracker_sidebar_container_collapse_dlc"> DLC</sup>}
-									</span>
-								</div>
-							))}
-						</Collapsible>
-					</div>
-				))}
-			</div>
-		);
 	}
 
 	return (
@@ -78,25 +30,56 @@ function Sidebar() {
 					</Link>
 				</div>
 			</div>
-			<div className="tracker_sidebar_tabview">
-				<div className="tracker_sidebar_tabview_tab" onClick={() => dispatch({ type: "TOGGLE_TAB", mast: "dm" })}>
-					<div className="tracker_sidebar_tabview_tab_title">
-						<Link to="/dm" onClick={closeSidebar}>
-							DM
-						</Link>
-					</div>
-					<div className="tracker_sidebar_tabview_tab_line" style={{ opacity: app.isActive === "dm" ? "1" : "0" }}></div>
-				</div>
-				<div className="tracker_sidebar_tabview_tab" onClick={() => dispatch({ type: "TOGGLE_TAB", mast: "da" })}>
-					<div className="tracker_sidebar_tabview_tab_title">
-						<Link to="/da" onClick={closeSidebar}>
-							DA
-						</Link>
-					</div>
-					<div className="tracker_sidebar_tabview_tab_line" style={{ opacity: app.isActive === "da" ? "1" : "0" }}></div>
-				</div>
+			<div className="tracker_sidebar_main">
+				<Tabs defaultActiveKey={app.isActive} id="uncontrolled-tab-example" onSelect={(k) => dispatch({ type: "TOGGLE_TAB", mast: k })}>
+					{["dm", "da"].map((mast) => {
+						const collapsibleId = mast === "dm" ? app.collapsibleIsOpen.dm : app.collapsibleIsOpen.da;
+						return (
+							<Tab
+								key={`tab-${mast}`}
+								eventKey={mast}
+								title={
+									<Link to={`/${mast}`} onClick={closeSidebar}>
+										{mast.toUpperCase()}
+									</Link>
+								}
+							>
+								{items.map((category, index) => {
+									return (
+										<Accordion
+											defaultActiveKey={collapsibleId[index] && "0"}
+											key={category.name + mast}
+											onClick={() =>
+												dispatch({
+													type: "TOGGLE_COLLAPSIBLE",
+													mast: mast,
+													id: index,
+												})
+											}
+										>
+											<Accordion.Item eventKey="0">
+												<Accordion.Header>{category.name.toUpperCase()}</Accordion.Header>
+												<Accordion.Body className="d-flex flex-column align-items-start">
+													{category.weapons.map((weapon) => {
+														return (
+															<div className="tracker_sidebar_main_item" key={weapon.name + mast}>
+																<Link to={"/weapon/" + mast + "_" + category.id + "_" + weapon.id} onClick={closeSidebar}>
+																	{weapon.name.toUpperCase()}
+																</Link>
+																{weapon.dlc && <sup className="tracker_sidebar_container_collapse_dlc"> DLC</sup>}
+															</div>
+														);
+													})}
+												</Accordion.Body>
+											</Accordion.Item>
+										</Accordion>
+									);
+								})}
+							</Tab>
+						);
+					})}
+				</Tabs>
 			</div>
-			<div className="tracker_sidebar_container">{app.isActive === "dm" ? <CollapsibleList mast="dm" /> : <CollapsibleList mast="da" />}</div>
 		</div>
 	);
 }
