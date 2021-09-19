7db847b6ac8ada421c720c70c5ac2d56341e7209
Hartaithan <hartaithan@gmail.com>
2021-09-19 12:53:15 +0900

    feat: add helper for generate tokens

---
+++ helpers/tokenHelper.js
@@ -0,0 +1,43 @@
+const { v4: uuid } = require("uuid");
+const jwt = require("jsonwebtoken");
+const Token = require("../models/token");
+
+function generateAccessToken(userId) {
+	return jwt.sign({ userId, type: "access" }, process.env.JWT_TOKEN, { expiresIn: "10s" });
+}
+
+function generateRefreshToken() {
+	const id = uuid();
+	return {
+		id,
+		token: jwt.sign({ id, type: "refresh" }, process.env.JWT_TOKEN, { expiresIn: "20s" }),
+	};
+}
+
+async function updateToken(tokenId, userId) {
+	const userToken = await Token.findOne({ userId });
+
+	if (!userToken) {
+		const newToken = new Token({ tokenId, userId });
+		await newToken.save();
+	} else {
+		await userToken.updateOne({ tokenId });
+	}
+}
+
+function generateTokens(userId) {
+	const access = generateAccessToken(userId);
+	const refresh = generateRefreshToken(userId);
+
+	return updateToken(refresh.id, userId).then(() => ({
+		token: access,
+		refresh_token: refresh.token,
+	}));
+}
+
+module.exports = {
+	generateAccessToken,
+	generateRefreshToken,
+	updateToken,
+	generateTokens,
+};
