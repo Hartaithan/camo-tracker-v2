a743710a53a8a46d204c48bc2193152baa2f80ae
Hartaithan <hartaithan@gmail.com>
2021-09-19 16:10:03 +0900

    feat: change token expiresTime

--- helpers/tokenHelper.js
+++ helpers/tokenHelper.js
@@ -3,14 +3,14 @@ const jwt = require("jsonwebtoken");
 const Token = require("../models/token");
 
 function generateAccessToken(userId) {
-	return jwt.sign({ userId, type: "access" }, process.env.JWT_TOKEN, { expiresIn: "20s" });
+	return jwt.sign({ userId, type: "access" }, process.env.JWT_TOKEN, { expiresIn: "15m" });
 }
 
 function generateRefreshToken() {
 	const id = uuid();
 	return {
 		id,
-		token: jwt.sign({ id, type: "refresh" }, process.env.JWT_TOKEN, { expiresIn: "40s" }),
+		token: jwt.sign({ id, type: "refresh" }, process.env.JWT_TOKEN, { expiresIn: "30d" }),
 	};
 }
 
