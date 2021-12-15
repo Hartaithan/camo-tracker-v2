fd40025d9c2e4756dfb34c9de37fc0fa6ccaaac7
Hartaithan <hartaithan@gmail.com>
2021-12-15 04:21:06 +0000

    feat: change select page layout

--- select/script.js
+++ select/script.js
@@ -1,1 +1,24 @@
+new Image().src = "img/vanguard-bg.jpg";
+new Image().src = "img/coldwar-bg.jpg";
 
+function setBackground(src) {
+  document.getElementById("wrapper").style.backgroundImage = `url(img/${src})`;
+}
+
+setBackground("vanguard-bg.jpg");
+
+document.getElementById("item-coldwar").addEventListener(
+  "mouseover",
+  function () {
+    setBackground("coldwar-bg.jpg");
+  },
+  false
+);
+
+document.getElementById("item-vanguard").addEventListener(
+  "mouseover",
+  function () {
+    setBackground("vanguard-bg.jpg");
+  },
+  false
+);
