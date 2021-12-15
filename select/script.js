fa1c020d8e96fe9ae3f5190de954654e406c1eb0
Hartaithan <hartaithan@gmail.com>
2021-12-15 04:23:53 +0000

    feat: vanguard item active by default on select page

--- select/script.js
+++ select/script.js
@@ -6,10 +6,14 @@ function setBackground(src) {
 }
 
 setBackground("vanguard-bg.jpg");
+const classList = document.getElementById("item-vanguard").classList;
 
 document.getElementById("item-coldwar").addEventListener(
   "mouseover",
   function () {
+    if (classList.contains("active")) {
+      classList.remove("active");
+    }
     setBackground("coldwar-bg.jpg");
   },
   false
@@ -18,6 +22,9 @@ document.getElementById("item-coldwar").addEventListener(
 document.getElementById("item-vanguard").addEventListener(
   "mouseover",
   function () {
+    if (classList.contains("active")) {
+      classList.remove("active");
+    }
     setBackground("vanguard-bg.jpg");
   },
   false
